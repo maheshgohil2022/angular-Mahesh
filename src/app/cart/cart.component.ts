38fa60419595c298c3f46024e45fd81dc1053edb
maheshgohil2022 <110255508+maheshgohil2022@users.noreply.github.com>
2022-11-18 21:07:17 -0500

    2 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -6,20 +6,19 @@ import { CartService } from '../cart.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
-
   items = this.cartService.getItems();
 
   checkoutForm = this.formBuilder.group({
     name: '',
-    address: ''
+    address: '',
   });
 
   constructor(
     private cartService: CartService,
-    private formBuilder: FormBuilder,
+    private formBuilder: FormBuilder
   ) {}
 
   onSubmit(): void {
